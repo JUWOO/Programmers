47f1e0d1f3861ba7e5bccc5d055cd441ec6d7e66
JUWOO <dearjuwoo@gmail.com>
2021-01-09 13:08:23 +0900

    210109

---
+++ level 2/가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(const int num1, const int num2)
+{
+    string s1 = to_string(num1) + to_string(num2);
+    string s2 = to_string(num2) + to_string(num1);
+    if(s1 < s2) return num2 > num1
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    
+    return answer;
+}
