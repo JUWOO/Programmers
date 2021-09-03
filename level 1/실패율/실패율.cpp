f4edc82f3a26286d82570052aa2e8cd67e793a11
JUWOO <55123253+JUWOO@users.noreply.github.com>
2021-09-03 20:01:01 +0900

    Update 실패율.cpp

--- level 1/실패율/실패율.cpp
+++ level 1/실패율/실패율.cpp
@@ -9,29 +9,51 @@ vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     int num = stages.size();
     vector<pair<float, int>> fail;
-    //vector <vector<int>> fail(n,vector<int>(2,0));
-    //vector<double> fail;
     vector<int> index;
     int count = num;
     sort(stages.begin(), stages.end());
     
-    //0-N+1
     for(int i=0 ; i<N+2 ; i++)
         index.push_back(0);
     
     for(int i=0; i<num; i++)
         index[stages[i]]++;
-    //return index;
+
     for(int i=1; i<N+1; i++)
     {
         count -= index[i-1];
-        double failure = (double)index[i]/count;
-        fail.push_back(pair<float, int>(failure, i));
+        if(index[i]==0)
+            fail.push_back(pair<float,int>(0,i));
+        else
+        {
+            double failure = (double)index[i]/count;
+            fail.push_back(pair<float, int>(failure, i));
+        }
     }
     
     sort(fail.rbegin(), fail.rend());
+    int c = 0;
+    vector<int> a;
     for(int i=0; i<N; i++)
-        answer.push_back(fail[i].second);
-
+    {
+        if(i<N-1 && fail[i].first == fail[i+1].first)
+        {
+            c++;
+            a.push_back(fail[i].second);
+        }
+        else
+            if(c > 0)
+            {
+                c++;
+                a.push_back(fail[i].second);
+                while(a.size()!=0)
+                {
+                    answer.push_back(a.back());
+                    a.pop_back();
+                }
+            }
+            else
+                answer.push_back(fail[i].second);
+    }
     return answer;
 }
